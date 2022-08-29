28da46556d8e93aa627e8e68f3eb6226134a7dd9
RodrigoOliveira-create <marmoresegranitos2017@gmail.com>
2022-08-29 00:58:21 -0300

    Atualizando

---
+++ calcularMedia.py
@@ -0,0 +1,7 @@
+numero1 = 3
+numero2 = 3
+numero3 = 3
+media = (numero1 + numero2 + numero3) / 3 # quando se coloca o parenteses se gera uma prioridade maior
+#somamedia = numero1 + numero2 + numero3 
+#media = somamedia / 3
+print(media)
